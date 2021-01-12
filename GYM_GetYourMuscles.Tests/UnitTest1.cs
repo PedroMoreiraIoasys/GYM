cff5ba04e73469af0f326a4bba3cc026c7eabc35
Pedro Moreira <pedromoreira@ioasys.com.br>
2021-01-12 14:31:18 -0300

    Insert test

---
+++ GYM_GetYourMuscles.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GYM_GetYourMuscles.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
