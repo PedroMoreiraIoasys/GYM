63793a546d0e826ccec81091b3345fa7227c700e
Pedro Moreira <pedromoreira@ioasys.com.br>
2021-01-20 12:50:07 -0300

    Fix Config

--- GYM_GetYourMuscles/Startup.cs
+++ GYM_GetYourMuscles/Startup.cs
@@ -61,12 +61,10 @@ namespace GYM_GetYourMuscles
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GYM_GetYourMuscles v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GYM_GetYourMuscles v1"));
+
 
             app.UseHttpsRedirection();
 
